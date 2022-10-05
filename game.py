2ec562825e6482fea4ae01a8a4087758760697ca
George Kent Allen <programmer@DESKTOP-7SBFESK.localdomain>
2022-10-05 08:41:35 -0700

    Basic game completed testing needed

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import statistics 
+import time
 import copy
 import pickle
 import random
@@ -10,18 +11,24 @@ from scipy.integrate import quad
 class proficiency:
     def __init__(self):
         self.avg = None # Average of last three times
-        self.missed = True # The number of correct answers before question is considered known
-        self.__lastThreeTimes = None
+        self.missed = 3 # The number of correct answers before question is considered known
+        self.__lastThreeTimes = []
 
     def newTime(self, time):
-        self.__lastThreeTimes.pop(0) 
+        if len(self.__lastThreeTimes) != 0:
+            self.__lastThreeTimes.pop(0) 
+
         self.__lastThreeTimes.append(time)
         if self.missed > 0:
             self.missed = self.missed-1;
-        self.__average(avg)
+            print("Missed", self.missed)
+        self.__average(self.avg)
+
+    def miss(self):
+        self.missed = 3
 
     def __average(self, avg):
-        mean(avg)
+        self.avg = statistics.mean(self.__lastThreeTimes)
         
 class integralMap:
     def __init__(self, numOfQuestions):
@@ -67,16 +74,22 @@ class pickleFriend:
         pickle_out.close()
         return 1
 
+def sortParam(card):
+    return card[1].avg
+        
+
 def sortByProficiency(cards):  
     numOfMissed = 0
     sortedCards = copy.copy(cards);
-    print("test")
+
     for x in range(0, len(cards)):
-        if cards[x][1].missed == True:     
+        if cards[x][1].missed > 0:     
             numOfMissed = numOfMissed + 1
             sortedCards.insert(0, sortedCards.pop(x))
-    sort(
+
+    sortedCards[numOfMissed:] = sorted(sortedCards[numOfMissed:],key = sortParam, reverse = True)
     return sortedCards
+
     
     
 
@@ -102,7 +115,41 @@ while(stageOne):
         cards = newCards()
         stageOne = False 
 
+distro = integralMap(len(cards))
 stageTwo = True
 while(stageTwo):
-    sortByProficiency(cards)
-    input("input")
+    cards = sortByProficiency(cards)
+    for x in cards:
+        print("[", x[1].missed, x[1].avg, "]", end =" ")
+
+    pairNum = distro.pickRandomIndex()
+    prof = cards[pairNum][1]
+    pair = cards[pairNum][0]
+
+    if random.uniform(0, 1) > .5: #50/50 for position
+        numOne = pair[1]
+        numTwo = pair[0]
+    else:
+        numOne = pair[0]
+        numTwo = pair[1]
+
+    print("\n\n",numTwo, "*", numOne)
+    start = time.time()
+    try:
+        answer = int(input())
+    except:
+        pf.save(cards)
+
+    if answer == numOne*numTwo:
+        print("!!!GREAT!!!")
+        elapse = time.time() - start 
+        prof.newTime(elapse)
+    else:
+        print("???BOO???")
+        print(numTwo, "*", numOne, "=", numOne*numTwo)
+        answer = input()
+        if answer == "save":
+            pf.save(cards)
+        else:
+            prof.miss()
+
